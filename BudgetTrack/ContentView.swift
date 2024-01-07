043762b3fa4491dbd00298b57f99d15adc1694c3
IgorVeLa <nigellaurente@yahoo.co.uk>
2024-01-07 18:06:07 +0000

    total function working with SwiftData now.

--- BudgetTrack/ContentView.swift
+++ BudgetTrack/ContentView.swift
@@ -37,7 +37,7 @@ struct ContentView: View {
                 Section {
                     HStack {
                         Text("Total: ")
-                        //Text(total(entries: entries, selectedMonth: selectedMonth), format: .currency(code: Locale.current.currency?.identifier ?? "GBP"))
+                        Text(total(entries: entries, selectedMonth: selectedMonth), format: .currency(code: Locale.current.currency?.identifier ?? "GBP"))
                     }
                 }
             }
@@ -61,20 +61,20 @@ struct ContentView: View {
         }
     }
     
-//    func total(entries: Entries, selectedMonth: String) -> Double {
-//        var incomes = [Double]()
-//        var expenses = [Double]()
-//
-//        for entry in entries.items {
-//            if entry.type == .income && entry.date.getMonthString() == selectedMonth {
-//                incomes.append(entry.amount)
-//            } else if entry.type == .expense && entry.date.getMonthString() == selectedMonth {
-//                expenses.append(entry.amount)
-//            }
-//        }
-//
-//        return (incomes.reduce(0, +)) - (expenses.reduce(0, +))
-//    }
+    func total(entries: [Entry], selectedMonth: String) -> Double {
+        var incomes = [Double]()
+        var expenses = [Double]()
+        
+        for entry in entries {
+            if entry.type == .income && entry.date.getMonthString() == selectedMonth {
+                incomes.append(entry.amount)
+            } else if entry.type == .expense && entry.date.getMonthString() == selectedMonth {
+                expenses.append(entry.amount)
+            }
+        }
+
+        return (incomes.reduce(0, +)) - (expenses.reduce(0, +))
+    }
 }
 
 #Preview {
